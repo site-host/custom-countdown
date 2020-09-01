d35b6197f55556932ba7191d8621b2ea4e5dc303
Rito <github@inklinq.ch>
2020-09-01 14:09:20 +0200

    save countdown in local storage

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ let countdownTitle = '';
 let countdownDate = '';
 let countdownValue = Date;
 let contdownActive;
+let savedCountdown;
 
 const secondInMil = 1000;
 const minuteInMil = secondInMil * 60;
@@ -66,6 +67,11 @@ function updateCountdown(e) {
     e.preventDefault();
     countdownTitle = e.srcElement[0].value;
     countdownDate = e.srcElement[1].value;
+    savedCountdown = {
+        title: countdownTitle,
+        date: countdownDate,
+    };
+    localStorage.setItem('countdown', JSON.stringify(savedCountdown));
     // Check for valid date
     if (countdownDate === '') {
         alert('Please select a date for the countdown.')
@@ -87,9 +93,25 @@ function resetCountdown() {
     // Reset values
     countdownTitle = '';
     countdownDate = '';
+    localStorage.removeItem('countdown');
+}
+
+function restorePreviousCountdown() {
+    // Get countdown from localSorage if available
+    if (localStorage.getItem('countdown')) {
+        inputContainer.hidden = true;
+        savedCountdown = JSON.parse(localStorage.getItem('countdown'));
+        countdownTitle = savedCountdown.title;
+        countdownDate = savedCountdown.date;
+        countdownValue = new Date(countdownDate).getTime();
+        updateDOM();
+    }
 }
 
 // Event Listener
 countdownForm.addEventListener('submit', updateCountdown);
 countdownBtn.addEventListener('click', resetCountdown);
 completeBtn.addEventListener('click', resetCountdown);
+
+// On Load, check localStorage
+restorePreviousCountdown();
